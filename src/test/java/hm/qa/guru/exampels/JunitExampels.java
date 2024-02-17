0b48945061795ca0097b1f124914348fd30f1098
ut.elaman <e.toktarov@p-s.kz>
2024-02-17 15:59:15 +0600

    Added Junit and Selenide

--- src/test/java/hm/qa/guru/exampels/JunitExampels.java
+++ src/test/java/hm/qa/guru/exampels/JunitExampels.java
@@ -1,31 +1,32 @@
+package hm.qa.guru.exampels;
+
 import org.junit.jupiter.api.*;
 
 public class JunitExampels {
 
     @BeforeAll
     static void setUtp(){
-
+        System.out.println("BeforeAll");
     }
 
     @AfterAll
     static void setOff(){
-
+        System.out.println("AfterAll");
     }
 
     @BeforeEach
     void beforeEach(){
-
+        System.out.println("    BeforeEach");
     }
 
     @AfterEach
     void afterEach(){
-
+        System.out.println("    AfterEach");
     }
 
 
     @Test
     void test(){
-        System.out.println("Testas ");
     }
 
 }
---
+++ src/test/java/hm/qa/guru/tests/TextBoxTests.java
@@ -0,0 +1,19 @@
+package hm.qa.guru.tests;
+
+import hm.qa.guru.exampels.JunitExampels;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class TextBoxTests extends JunitExampels {
+
+
+
+    @Test
+    public void openTest() {
+        open("https://demoqa.com");
+        System.out.println("        openTest");
+        sleep(4000);
+    }
+}
